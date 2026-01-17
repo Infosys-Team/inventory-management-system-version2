3f5bd6e6ab04e16f6fb421f4071ed3ef2c08ef1f
Nitish Chaudhary <166990109+nitish7272@users.noreply.github.com>
2026-01-17 22:50:56 +0530

    Update SecurityConfig.java

--- src/main/java/com/inventory/config/SecurityConfig.java
+++ src/main/java/com/inventory/config/SecurityConfig.java
@@ -1,22 +1,14 @@
-// package com.inventory.security;
+package com.inventory.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-// @Configuration
-// public class SecurityConfig {
+@Configuration
+public class SecurityConfig {
 
-//     @Bean
-//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-//         http
-//             .csrf(csrf -> csrf.disable())
-//             .authorizeHttpRequests(auth -> auth
-//                 .requestMatchers("/**").permitAll()
-//             );
-
-//         return http.build();
-//     }
-// }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
